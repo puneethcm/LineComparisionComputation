ac994bfeef841e52ef7238143eaaa8236da19587
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-21 20:27:01 +0530

    Distance bte tow lines

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -2,7 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to line Comparision computation");
+        Console.WriteLine("Welcome line comparision");
+        double x1 = 1;
+        double y1 = 1;
+        double x2 = 5;
+        double y2 = 5;
+        double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        Console.WriteLine("distance btw two lines" + distance);
         Console.ReadLine();
     }
 }
