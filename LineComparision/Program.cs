55e464ff34510aec540a9951c32ef2088a758af2
puneethcm <puneethkumarcm8050@gmail.com>
2023-01-22 18:50:27 +0530

    Comparing two lines

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -9,8 +9,29 @@
         Console.WriteLine("Enter the value of x2, y2:");
         int x2 = Convert.ToInt32(Console.ReadLine());
         int y2 = Convert.ToInt32(Console.ReadLine());
-        double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        Console.WriteLine("Length of two lines" + length);
+        double length1 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+        Console.WriteLine("Welcome line comparision problems");
+        Console.WriteLine("Enter the value of x3, y3:");
+        int x3 = Convert.ToInt32(Console.ReadLine());
+        int y3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the value of x4, y4:");
+        int x4 = Convert.ToInt32(Console.ReadLine());
+        int y4 = Convert.ToInt32(Console.ReadLine());
+        double length2 = Math.Sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+
+        if (length1 > length2)
+        {
+            Console.WriteLine("Line 1 is greater than Line 2");
+        }
+        else if (length2 > length1)
+        {
+            Console.WriteLine("Line 2 is greater than Line 1");
+        }
+        else
+        {
+            Console.WriteLine("The Two lines are equal");
+        }
         Console.ReadLine();
     }
 }
